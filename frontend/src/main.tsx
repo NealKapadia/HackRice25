ca88b768976794771a7b4b20d7556490a5c50422
NealKapadia <nkapadia15@outlook.com>
2025-09-20 22:31:35 -0500

    Revert "feat(auth): wrap app with Auth0Provider using provided domain/clientId and add AuthenticationButton"
    This reverts commit 7a33252e57f2796efa7a32e7865859b9c146dafe.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,21 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Auth0Provider } from '@auth0/auth0-react'
-
-const domain = 'dev-1gv3rl5fk1sti30x.us.auth0.com'
-const clientId = 'rxEcMtbcySIteA1Bz4tzud7GERChntbJ'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </StrictMode>,
 )
